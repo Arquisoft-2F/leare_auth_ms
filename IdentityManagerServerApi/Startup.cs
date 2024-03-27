3880ec9dbb15aae9fa67a14ff7efb7da0e136edf
Esteban López <eslopezb@unal.edu.co>
2024-03-26 22:53:07 -0500

    Startup added

---
+++ IdentityManagerServerApi/Startup.cs
@@ -0,0 +1,21 @@
+﻿using IdentityManagerServerApi.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+namespace auth_ms_api
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            var _db = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
+            if (_db != null)
+            {
+                if (_db.Database.GetPendingMigrations().Any())
+                {
+                    _db.Database.Migrate();
+                }
+            }
+        }
+    }
+}
